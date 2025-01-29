c1b283a90b3c77e1c29e860d211edc48cf51d0c7
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-01-29 22:55:10 +0100

    Update application

--- application/main.cpp
+++ application/main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char * argv[])
     auto parser = argparse::ArgumentParser();
     parser.add_argument("positional");
     parser.add_argument("--optional");
-    parser.add_argument("-v", "--version").action(argparse::version).version("prealpha");
+    parser.add_argument("-v", "--version").action(argparse::version).version("1.0.0");
     parser.parse_args(argc, argv);
 }
